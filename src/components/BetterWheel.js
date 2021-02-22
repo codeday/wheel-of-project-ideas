8e8cf027132e719dcebe32004fc713e9e209ccdb
Tyler Menezes <tylermenezes@codeday.org>
2021-02-22 17:47:17 -0500

    Make the wheel of fun spin up and down

---
+++ src/components/BetterWheel.js
@@ -0,0 +1,212 @@
+import Box from '@codeday/topo/Atom/Box';
+import { Component, createRef } from 'react';
+
+const PI2 = Math.PI * 2;
+const RAD_PER_DEG = Math.PI / 180;
+
+export default class BetterWheel extends Component {
+  constructor(props) {
+    super(props);
+    const { minDegPerSec, maxDegPerSec, times, size } = props;
+
+    const minRadPerMs = (minDegPerSec / 1000) * RAD_PER_DEG;
+    const maxRadPerMs = (maxDegPerSec / 1000) * RAD_PER_DEG;
+
+    this.drawSize = (size/2) - 5;
+    this.centerX = this.drawSize + 5;
+    this.centerY = this.drawSize + 5;
+
+    this.angleCurrent = Math.random() * PI2;
+    this.isSpinning = false;
+
+    this.canvas = createRef();
+    this.spin = this.spin.bind(this);
+    this.frame = this.frame.bind(this);
+    this.draw = this.draw.bind(this);
+    this.drawNeedle = this.drawNeedle.bind(this);
+    this.drawSegment = this.drawSegment.bind(this);
+    this.drawWheel = this.drawWheel.bind(this);
+    this.clear = this.clear.bind(this);
+
+    let elapsed = 0;
+    this.tweenTimes = [
+      { time: 0, radPerMs: 0 },
+      ...times.slice(0, times.length - 1).map((time, i) => ({
+        time: (elapsed = elapsed + time),
+        radPerMs: [maxRadPerMs, maxRadPerMs, minRadPerMs, minRadPerMs][(i + 1) % 4],
+      })),
+      { time: elapsed + times[times.length - 1], radPerMs: 0 },
+    ];
+  }
+
+  render() {
+    const { size, ...rest } = this.props;
+
+    return (
+      <Box
+        as="canvas"
+        ref={this.canvas}
+        width={size}
+        height={size}
+        d="inline-block"
+        onClick={this.spin}
+        {...rest}
+      />
+    )
+  }
+
+  componentDidMount() {
+    const { size } = this.props;
+
+    if (this.canvas?.current) {
+      this.ctx = this.canvas.current.getContext('2d');
+      this.canvas.current.setAttribute('width', size)
+      this.canvas.current.setAttribute('height', size)
+      this.frame();
+    }
+  }
+
+  spin() {
+    this.spinStartTime = new Date().getTime();
+    this.lastFrameTime = new Date().getTime();
+    this.isSpinning = true;
+    this.props.onSpinning && this.props.onSpinning();
+    this.frame();
+  }
+
+  frame() {
+    const { onComplete } = this.props;
+    if (!this.isSpinning) return this.draw();
+
+    const now = new Date().getTime();
+    const elapsedTime = !this.spinStartTime ? 0 : now - this.spinStartTime;
+    const frameElapsedTime = !this.lastFrameTime ? 0 : now - this.lastFrameTime;
+
+    // Interpolation to get current speed
+    const tweenIndex = Math.max(1, this.tweenTimes.filter((t) => t.time < elapsedTime).length);
+    const tweenPrevious = this.tweenTimes[tweenIndex - 1];
+    const tweenNext = this.tweenTimes[tweenIndex];
+
+    if (tweenNext) {
+      const tweenDiff = tweenNext.radPerMs - tweenPrevious.radPerMs;
+      const tweenPercent = (elapsedTime - tweenPrevious.time) / (tweenNext.time - tweenPrevious.time);
+      const deltaAngle = ((tweenPercent * tweenDiff) + tweenPrevious.radPerMs) * frameElapsedTime;
+      this.angleCurrent = (this.angleCurrent + deltaAngle) % PI2;
+    } else {
+      this.isSpinning = false;
+      onComplete && onComplete();
+    }
+
+    this.lastFrameTime = now;
+    this.draw();
+    window?.requestAnimationFrame(this.frame);
+  }
+
+  draw() {
+    this.clear();
+    this.drawWheel();
+    this.drawNeedle();
+  }
+
+  drawSegment (key, lastAngle, angle) {
+    const { ctx, centerX, centerY, drawSize } = this;
+    const { segments, colors, contrastColor, fontSize, font } = this.props;
+    const value = segments[key];
+
+    ctx.save();
+    ctx.beginPath();
+    ctx.moveTo(centerX, centerY);
+    ctx.arc(centerX, centerY, drawSize, lastAngle, angle, false);
+    ctx.lineTo(centerX, centerY);
+    ctx.closePath();
+    ctx.fillStyle = colors[key % colors.length];
+    ctx.fill();
+    ctx.stroke();
+    ctx.save();
+    ctx.translate(centerX, centerY);
+    ctx.rotate((lastAngle + angle) / 2);
+    ctx.fillStyle = contrastColor || 'white';
+    ctx.font = `bold ${fontSize} ${font}`;
+    ctx.fillText(value.substr(0, 21), drawSize / 2 + 20, 0);
+    ctx.restore();
+  }
+
+  drawWheel() {
+    const { ctx, centerX, centerY, drawSize, angleCurrent } = this;
+    const { segments, primaryColor, contrastColor, buttonText, font, fontSize } = this.props;
+
+    let lastAngle = angleCurrent;
+    const len = segments.length
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = primaryColor || 'black';
+    ctx.textBaseline = 'middle';
+    ctx.textAlign = 'center';
+    ctx.font = `${fontSize} ${font}`;
+
+    // Draw segments
+    for (let i = 1; i <= len; i++) {
+      const angle = PI2 * (i / len) + angleCurrent;
+      this.drawSegment(i - 1, lastAngle, angle);
+      lastAngle = angle;
+    }
+
+    // Draw a center circle
+    ctx.beginPath()
+    ctx.arc(centerX, centerY, 50, 0, PI2, false)
+    ctx.closePath()
+    ctx.fillStyle = primaryColor || 'black'
+    ctx.lineWidth = 10
+    ctx.strokeStyle = contrastColor || 'white'
+    ctx.fill()
+    ctx.font = `bold ${fontSize} ${font}`
+    ctx.fillStyle = contrastColor || 'white'
+    ctx.textAlign = 'center'
+    ctx.fillText(buttonText || 'Spin', centerX, centerY + 3)
+    ctx.stroke()
+
+    // Draw outer circle
+    ctx.beginPath()
+    ctx.arc(centerX, centerY, drawSize, 0, PI2, false)
+    ctx.closePath()
+
+    ctx.lineWidth = 10
+    ctx.strokeStyle = primaryColor || 'black'
+    ctx.stroke()
+  }
+
+  drawNeedle() {
+    const { ctx, centerX, centerY, angleCurrent } = this;
+    const { font, primaryColor, contrastColor, segments, onUpdate } = this.props;
+
+    ctx.lineWidth = 1
+    ctx.strokeStyle = contrastColor || 'white'
+    ctx.fileStyle = contrastColor || 'white'
+    ctx.beginPath()
+    ctx.moveTo(centerX + 20, centerY - 50)
+    ctx.lineTo(centerX - 20, centerY - 50)
+    ctx.lineTo(centerX, centerY - 70)
+    ctx.closePath()
+    ctx.fill()
+    const change = angleCurrent + Math.PI / 2
+    let i =
+      segments.length -
+      Math.floor((change / (Math.PI * 2)) * segments.length) -
+      1
+    if (i < 0) i = i + segments.length
+    ctx.textAlign = 'center'
+    ctx.textBaseline = 'middle'
+    ctx.fillStyle = primaryColor || 'black'
+    ctx.font = `bold 1.5em ${font}`
+
+    if (segments[i] !== this.previousSegment) {
+      this.previousSegment = segments[i];
+      onUpdate && onUpdate(segments[i]);
+    }
+  }
+
+  clear() {
+    const { size } = this.props;
+
+    this.ctx.clearRect(0, 0, size, size)
+  }
+}
--- src/pages/[duration]/[fakeouts]/[items].js
+++ src/pages/[duration]/[fakeouts]/[items].js
@@ -1,28 +1,54 @@
 import { useState, useMemo, useEffect, useRef } from 'react';
 import Head from 'next/head';
 import useWindowSize from 'react-use/lib/useWindowSize';
+import { useTheme } from '@codeday/topo/utils';
 import Confetti from 'react-confetti';
 import Box from '@codeday/topo/Atom/Box';
-import Wheel from '../../components/Wheel';
-import ControlledAudioPlayer from '../../components/ControlledAudioPlayer';
+import BetterWheel from '../../../components/BetterWheel';
+import ControlledAudioPlayer from '../../../components/ControlledAudioPlayer';
 
-export default function IndexPage({ duration, items }) {
+const INITIAL_SPIN_UP = 50;
+const FINAL_SPIN = 1000;
+const FINAL_DOWN = 1000;
+const COLORS = [ 'red', 'orange', 'green', 'blue', 'purple' ];
+const SHADE = 500;
+
+export default function IndexPage({ duration, fakeouts, items }) {
   const [hasStarted, setHasStarted] = useState(false);
   const [hasLoaded, setHasLoaded] = useState(false);
   const [selected, setSelected] = useState();
   const [winner, setWinner] = useState();
   const { width, height } = useWindowSize();
+  const { colors } = useTheme();
 
+  const perCycle = Math.max(3000, ((duration / fakeouts) * 1000) - (INITIAL_SPIN_UP + FINAL_SPIN + FINAL_DOWN));
+  const c_d_c_u = [perCycle * 0.35, perCycle * 0.2, perCycle * 0.05, perCycle * 0.4];
+
+  const mappedColors = COLORS.map((c) => colors[c][SHADE]);
+  const spinPattern = [
+    INITIAL_SPIN_UP,
+    ...Array(Number.parseInt(fakeouts)).fill(c_d_c_u).reduce((a, b) => [...a, ...b], []),
+    FINAL_SPIN,
+    FINAL_DOWN,
+  ];
   const wheelSize = height - 200;
 
   const wheel = useMemo(() => (
-    <Wheel
-      items={items.split(',')}
+    <BetterWheel
+      segments={items.split(',')}
+      colors={mappedColors}
       onUpdate={setSelected}
-      onWinner={setWinner}
-      onStartedSpinning={() => setHasStarted(true)}
-      duration={duration * 1000}
+      onComplete={() => setWinner(true)}
+      onSpinning={() => setHasStarted(true)}
+      maxDegPerSec={400}
+      minDegPerSec={45}
+      times={spinPattern}
       size={wheelSize}
+      font="Sofia Pro"
+      fontSize="2.5vh"
+      buttonText='SPIN'
+      primaryColor={colors.black}
+      contrastColor={colors.white}
     />
   ), [setSelected, setWinner, setHasStarted, duration, items, hasLoaded, wheelSize]);
 
